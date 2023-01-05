075ff03d073c64ba2617ee88573c8bee80158fcc
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-01-05 18:00:02 -0300

    calculadora

---
+++ calculadora/calculadora.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void) {
+    int n1, n2, op;
+
+    printf("Digite o primeiro número: ");
+    scanf("%d", &n1);
+
+    printf("Digite o segundo número: ");
+    scanf("%d", &n2);
+
+    printf("Digite a operação desejada: \n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n");
+    scanf("%d", &op);
+
+    switch(op) {
+        case 1:
+            printf("Soma: %d", n1 + n2);
+            break;
+        case 2:
+            printf("Subtração: %d", n1 - n2);
+            break;
+        case 3:
+            printf("Multiplicação: %d", n1 * n2);
+            break;
+        case 4:
+            printf("Divisão: %d", n1 / n2);
+            break;
+        default:
+            printf("Opção inválida");
+    }
+
+}
+
+
