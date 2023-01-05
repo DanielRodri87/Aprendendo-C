2eabad8bdb33619919f387883da46d2db6849d1f
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-01-05 18:18:25 -0300

    atualização calculadora

--- calculadora/calculadora.c
+++ calculadora/calculadora.c
@@ -1,33 +1,53 @@
 #include <stdio.h>
 
 int main(void) {
-    int n1, n2, op;
 
-    printf("Digite o primeiro número: ");
-    scanf("%d", &n1);
+    int contador;
+    for (contador = 0; contador == 0;) {
 
-    printf("Digite o segundo número: ");
-    scanf("%d", &n2);
+        int n1, n2, op;
+        printf("\n=============================================================================\n");
+        printf("Digite o primeiro número: ");
+        scanf("%d", &n1);
 
-    printf("Digite a operação desejada: \n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n");
-    scanf("%d", &op);
+        printf("Digite o segundo número: ");
+        scanf("%d", &n2);
+        printf("\n=============================================================================\n");
 
-    switch(op) {
-        case 1:
-            printf("Soma: %d", n1 + n2);
-            break;
-        case 2:
-            printf("Subtração: %d", n1 - n2);
-            break;
-        case 3:
-            printf("Multiplicação: %d", n1 * n2);
-            break;
-        case 4:
-            printf("Divisão: %d", n1 / n2);
-            break;
-        default:
-            printf("Opção inválida");
+        printf("Digite a operação desejada: \n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n");
+        scanf("%d", &op);
+
+        switch(op) {
+            case 1:
+                printf("Soma: %d", n1 + n2);
+                break;
+            case 2:
+                printf("Subtração: %d", n1 - n2);
+                break;
+            case 3:
+                printf("Multiplicação: %d", n1 * n2);
+                break;
+            case 4:
+                printf("Divisão: %d", n1 / n2);
+                break;
+            default:
+                printf("Opção inválida");
+                contador = 1;
+        }
+
+
+        int saida;
+        printf("\n=============================================================================\n");
+        printf("\nDeseja realizar outra operação? \n1 - Sim\n2 - Não\n");
+        scanf("%d", &saida);
+
+        if (saida == 2) {
+            contador = 1;
+        }
+
     }
+
+    return 0;
 
 }
 
