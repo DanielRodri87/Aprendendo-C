42025e5d13ef7434f74265db18a86c6f778e4b94
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-08-22 11:37:14 -0300

    att

--- FERIAS/beecrownd/output/questao5.c
+++ FERIAS/beecrownd/output/questao5.c
@@ -1,28 +1,19 @@
+
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
-    int id[100], i;
-    char matriz[100][3], cidade; 
+    char nome[100];
+    double salario, vendas, total;
 
-    for (i = 0; i < 100; i++)
-    {
-        gets(matriz[i][0]); 
-        gets(matriz[i][1]); 
-        gets(matriz[i][2]); 
+    scanf("%s", nome);
+    scanf("%lf", &salario);
+    scanf("%lf", &vendas);
 
-    }
-    for (i = 0; i < 100; i++)
-    {
-        if (cidade == matriz[i][2])
-        {
-            continue;
-        } else
-        {
-            printf(cidade);
-        }
-    }
+    total = salario + (vendas * 0.15);
+
+    printf("TOTAL = R$ %.2lf\n", total);
+
 
     return 0;
 }
