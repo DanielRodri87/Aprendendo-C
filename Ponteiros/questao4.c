b920083664d900a60d31db943aa718beec8ef98c
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-09-12 17:13:45 -0300

    ponteiros + funções

---
+++ Ponteiros/questao4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void troca(int *a, int *b)
+{
+    int aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
+int main(void)
+{
+    int valor1, valor2;
+    scanf("%d %d", &valor1, &valor2);
+
+    int *pont1 = &valor1, *pont2 = &valor2;
+    
+    printf("\n\nAntes da troca:\n");
+    printf("Valor de A: %d\n", *pont1);
+    printf("Valor de B: %d\n", *pont2);
+
+    troca(pont1, pont2);
+
+    printf("\n\nDepois da troca:\n");
+    printf("Valor de A: %d\n", *pont1);
+    printf("Valor de B: %d\n", *pont2);
+    getchar();
+    return 0;
+   
+}
