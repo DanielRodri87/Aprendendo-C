fcb22cebe6d8faa2a055878a43c474f0c5f1534c
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-10-18 19:43:11 -0300

    att

---
+++ funcao/ListaRita/questao3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define max 5
+
+struct Aluno
+{
+    char nome[50];
+    int matricula;
+    char curso[50];
+};
+
+int main()
+{
+    struct Aluno a1[max];
+
+    for (int i = 0; i < max; i++)
+    {
+        printf("Digite o nome do %d Aluno: ", i+1);
+        scanf("%s", a1[i].nome);
+        printf("Digite a matricula do %d Aluno: ", i+1);
+        scanf("%d", &a1[i].matricula);
+        printf("Digite o curso do %d Aluno: ", i+1);
+        scanf("%s", a1[i].curso);
+    }
+
+    for (int i = 0; i < max; i++)
+    {
+        printf("Nome: %s\n", a1[i].nome);
+        printf("Matricula: %d\n", a1[i].matricula);
+        printf("Curso: %s\n", a1[i].curso);
+    }
+
+    return 0;
+}
