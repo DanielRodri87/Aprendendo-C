2de88df806101cf859606bcc7d27507c1a7c038a
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-12-27 17:40:01 -0300

    att

---
+++ calculadora.java
@@ -0,0 +1,24 @@
+public class calculadora
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Digite os valores ");
+        int op = Integer.parseInt(System.console().readLine());
+        System.out.println("Digite o primeiro valor: ");
+        int a = Integer.parseInt(System.console().readLine());
+        System.out.println("Digite o segundo valor: ");
+        int b = Integer.parseInt(System.console().readLine());
+
+        if (op == 1)
+            System.out.println("Resultado: " + (a + b));
+        else if (op == 2)
+            System.out.println("Resultado: " + (a - b));
+        else if (op == 3)
+            System.out.println("Resultado: " + (a * b));
+        else if (op == 4)
+            System.out.println("Resultado: " + (a / b));
+        else
+            System.out.println("Operacao invalida!");
+
+    }
+}
--- teste.py
+++ teste.py
@@ -1,4 +1,28 @@
-a = int(input())
-b = int(input())
-s = a+b
-print(f"SOMA = {s}")
+class Pessoa:
+    def __init__(self, nome, idade, cpf, matricula):
+        self.nome = nome
+        self.idade = idade
+        self.cpf = cpf
+        self.matricula = matricula
+        
+    
+    
+    def cadastrarAluno(self):
+        name = input("Digite o nome do aluno: ")
+        age = input("Digite a idade do aluno: ")
+        cpf = input("Digite o cpf do aluno: ")
+        matricula = input("Digite a matricula do aluno: ")
+        aluno = Pessoa(name, age, cpf, matricula)
+        
+        print("Aluno cadastrado com sucesso!")
+        return aluno
+
+
+
+
+if __name__ == "__main__":
+    aluno = Pessoa()
+    aluno.cadastrarAluno()
+    
+    
+        
